aa7d9905f5ee390c0c9b6c9f6b0d72dcf2a3089c
Katrina Frichette <kat.frichette@gmail.com>
2026-05-18 15:29:51 -0600

    Added pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,6 +12,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term: string) => {
     console.log('Searching for:', term);
     const params = new URLSearchParams(searchParams.toString());
+    params.set('page', '1'); // Reset to the first page on new search
 
     if (term) {
       params.set('query', term);
